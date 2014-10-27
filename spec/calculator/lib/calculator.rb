b6d5972a883b4fac09c9f5348bbcf41510047171
Ivan Sim <ihcsim@gmail.com>
2014-10-27 00:31:04 +0000

    More tests for addition operation.

--- spec/calculator/lib/calculator.rb
+++ spec/calculator/lib/calculator.rb
@@ -1,5 +1,9 @@
 class Calculator
-  def add(a,b)
-    a + b
+  def add(*operands)
+    result = 0
+    operands.each do |operand|
+      result += operand
+    end
+    result
   end
 end
--- spec/calculator/lib/calculator_spec.rb
+++ spec/calculator/lib/calculator_spec.rb
@@ -1,9 +1,34 @@
 require './calculator'
 
 RSpec.describe Calculator do
+  before(:each) do
+    @calculator = Calculator.new
+  end
+
   describe '#add' do
-    it 'returns the sum of its arguments' do
-      expect(Calculator.new.add(1, 2)).to eq(3)
+    context "with at least two arguments" do
+      it 'returns the sum of its arguments' do
+        expect(@calculator.add(1, 2)).to eq(3)
+        expect(@calculator.add(11, 21)).to eq(32)
+        expect(@calculator.add(76, 21)).to eq(97)
+        expect(@calculator.add(1, 2, 3)).to eq(6)
+        expect(@calculator.add(11, 21, 13)).to eq(45)
+        expect(@calculator.add(76, 21, 51)).to eq(148)
+        expect(@calculator.add(1, 2, 3, 4)).to eq(10)
+        expect(@calculator.add(11, 21, 13, 19)).to eq(64)
+        expect(@calculator.add(76, 21, 51, 33)).to eq(181)
+
+        # some negative operands
+        expect(@calculator.add(-5, -9)).to eq(-14)
+        expect(@calculator.add(11, -21)).to eq(-10)
+        expect(@calculator.add(-76, 17)).to eq(-59)
+        expect(@calculator.add(-1, -2, 3)).to eq(0)
+        expect(@calculator.add(-20, 21, 13)).to eq(14)
+        expect(@calculator.add(-60, 70, 13)).to eq(23)
+        expect(@calculator.add(-1, -2, -3, -4)).to eq(-10)
+        expect(@calculator.add(20, -33, 13, 0)).to eq(0)
+        expect(@calculator.add(71, -21, -51, 33)).to eq(32)
+      end
     end
   end
 end
