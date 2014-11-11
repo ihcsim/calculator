858d6e65339275401740a803fb4cb2b9225483dd
Ivan Sim <ihcsim@gmail.com>
2014-11-11 05:37:21 +0000

    Add 'increment' private method.

--- spec/calculator/lib/arithmetic.rb
+++ spec/calculator/lib/arithmetic.rb
@@ -2,7 +2,7 @@ module Arithmetic
   def addition(operands)
     result = 0
     operands.each do |operand|
-      result += operand
+      result = increment result, operand
     end
     result
   end
@@ -10,8 +10,13 @@ module Arithmetic
   def subtraction(operands)
     result = operands.shift
     operands.each do |operand|
-      result -= operand
+      result = increment result, -1 * operand
     end
     result
+  end
+
+  private
+  def increment(operand1, operand2)
+    operand1 + operand2
   end
 end
