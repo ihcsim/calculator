2497f246515bc89514e515b9de580cfb4c571874
Ivan Sim <ihcsim@gmail.com>
2014-11-11 23:45:46 +0000

    Extract spy injection and removal to into 'before(:all)' and after(:all)'.

--- spec/calculator/lib/calculator_arithmetic_spec.rb
+++ spec/calculator/lib/calculator_arithmetic_spec.rb
@@ -1,12 +1,18 @@
 RSpec.describe 'Calculator Arithmetic Spy' do
+  before(:all) do
+    ArithmeticSpy.spy_increment
+  end
+
+  after(:all) do
+    ArithmeticSpy.remove_spy
+  end
+
   describe 'spy increment method' do
     context 'when addition is invoked' do
       it 'all operands are added' do
-        ArithmeticSpy.spy_increment
         calculator = Calculator.new
         calculator.add(1,2)
         expect(calculator.spy_result).to eq("1 + 2")
-        ArithmeticSpy.remove_spy
       end
     end
   end
