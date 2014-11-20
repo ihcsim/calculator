16d04271bd0b5d7a5229b658a4b77fe1a50514b0
Ivan Sim <ihcsim@gmail.com>
2014-11-20 06:31:15 +0000

    Product specs with more operands.

--- spec/arithmetic_product_spec.rb
+++ spec/arithmetic_product_spec.rb
@@ -26,6 +26,18 @@ RSpec.describe Arithmetic do
         calculator.multiply(89, 46)
         expect(calculator.spy_result).to eq('89*46=4094')
 
+        calculator.multiply(3, 4, 5)
+        expect(calculator.spy_result).to eq('3*4*5=60')
+
+        calculator.multiply(22, 45, 78)
+        expect(calculator.spy_result).to eq('22*45*78=77220')
+
+        calculator.multiply(145, 105, 199)
+        expect(calculator.spy_result).to eq('145*105*199=3029775')
+
+        calculator.multiply(231, 333, 523)
+        expect(calculator.spy_result).to eq('231*333*523=40230729')
+
       end
     end
   end
--- spec/arithmetic_product_spy.rb
+++ spec/arithmetic_product_spy.rb
@@ -3,11 +3,16 @@ class ArithmeticProductSpy
     Arithmetic.class_eval do
       alias_method :original_product, :product
       define_method :product do |operand1, operand2|
-        @spy = "#{operand1}*#{operand2}=#{original_product operand1, operand2}"
+        @spy ||= ''
+        @spy = operand1.to_s if @spy.empty?
+        @spy << "*#{operand2}"
+        @result = original_product operand1, operand2
       end
 
       define_method :spy_result do
-        @spy
+        spy_result = @spy + "=#{@result}"
+        @spy.clear
+        spy_result
       end
     end
   end
