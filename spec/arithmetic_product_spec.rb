7b06a2a1b1118fcc287d45a6ecd8f8742d54db40
Ivan Sim <ihcsim@gmail.com>
2014-11-21 05:26:25 +0000

    More specs to spy the multiplication operation.

--- spec/arithmetic_product_spec.rb
+++ spec/arithmetic_product_spec.rb
@@ -20,14 +20,14 @@ RSpec.describe Arithmetic do
         calculator.multiply(11, 22)
         expect(calculator.spy_result).to eq('11*22=242')
 
-        calculator.multiply(50, 67)
-        expect(calculator.spy_result).to eq('50*67=3350')
+        calculator.multiply(-50, 67)
+        expect(calculator.spy_result).to eq('-50*67=-3350')
 
         calculator.multiply(89, 46)
         expect(calculator.spy_result).to eq('89*46=4094')
 
-        calculator.multiply(3, 4, 5)
-        expect(calculator.spy_result).to eq('3*4*5=60')
+        calculator.multiply(3, -4, 5)
+        expect(calculator.spy_result).to eq('3*-4*5=-60')
 
         calculator.multiply(22, 45, 78)
         expect(calculator.spy_result).to eq('22*45*78=77220')
@@ -37,6 +37,18 @@ RSpec.describe Arithmetic do
 
         calculator.multiply(231, 333, 523)
         expect(calculator.spy_result).to eq('231*333*523=40230729')
+
+        calculator.multiply(3, 3, 3, 3)
+        expect(calculator.spy_result).to eq('3*3*3*3=81')
+
+        calculator.multiply(0, -14, 37, 52)
+        expect(calculator.spy_result).to eq('0*-14*37*52=0')
+
+        calculator.multiply(94, 113, 189, 340)
+        expect(calculator.spy_result).to eq('94*113*189*340=682569720')
+
+        calculator.multiply(107, 176, 222, 456)
+        expect(calculator.spy_result).to eq('107*176*222*456=1906401024')
 
       end
     end
